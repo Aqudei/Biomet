9740fbc6fe70c460d60a47a8a8ab60048adfdeb6
Aqudei <fdeu.aalmine@gmail.com>
2018-02-28 21:20:32 +0800

    sss

--- Biomet/ViewModels/DTRViewModel.cs
+++ Biomet/ViewModels/DTRViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using DPFP;
 using DPFP.Verification;
 
@@ -16,12 +19,51 @@ namespace Biomet.ViewModels
         {
             base.Init();
             Verificator = new DPFP.Verification.Verification();
+
+            LoadTemplates();
+        }
+
+        private Dictionary<string, Template> _templates = new Dictionary<string, Template>();
+
+        private void LoadTemplates()
+        {
+            var files = Directory.GetFiles(Properties.Settings.Default.FPTEMPLATE_DIR);
+            foreach (var f in files)
+            {
+                var templateBytes = File.ReadAllBytes(f);
+                using (var mem = new MemoryStream(templateBytes))
+                {
+                    var template = new Template();
+                    template.DeSerialize(mem);
+                    _templates.Add(Path.GetFileNameWithoutExtension(f), template);
+                }
+
+            }
         }
 
         protected override void Process(Sample Sample)
         {
             base.Process(Sample);
+            var features = ExtractFeatures(Sample, DPFP.Processing.DataPurpose.Verification);
+            if (features == null) return;
 
+            var result = new DPFP.Verification.Verification.Result();
+            foreach (var storedTemplate in _templates)
+            {
+                Verificator.Verify(features, storedTemplate.Value, ref result);
+                UpdateStatus(result.FARAchieved);
+
+                if (result.Verified)
+                {
+                    MessageBox.Show("[Temporary only for debug]\nYou are " + storedTemplate.Key);
+                    return;
+                }
+            }
+        }
+
+        private void UpdateStatus(int fARAchieved)
+        {
+            Debug.WriteLine("Far Achieved: " + fARAchieved);
         }
     }
 }
